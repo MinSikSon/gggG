0fa67672e081b3e26eff0a411eddf1c6f0e5c4d0
MinSikSon <tofu89223@gmail.net>
2015-01-19 23:03:56 +0900

    change a lot of code about achartengine

--- src/son/funkydj3/achartengine/Chart1.java
+++ src/son/funkydj3/achartengine/Chart1.java
@@ -11,27 +11,73 @@ import org.achartengine.GraphicalView;
 import org.achartengine.chart.BarChart.Type;
 import org.achartengine.model.XYMultipleSeriesDataset;
 import org.achartengine.model.XYSeries;
+import org.achartengine.renderer.SimpleSeriesRenderer;
 import org.achartengine.renderer.XYMultipleSeriesRenderer;
 import org.achartengine.renderer.XYSeriesRenderer;
 
 import son.funkydj3.gggg.R;
 import android.app.Activity;
+import android.graphics.Color;
+import android.graphics.Paint.Align;
 import android.os.Bundle;
 import android.widget.LinearLayout;
 
 public class Chart1 extends Activity {
 	private GraphicalView mChart; // * Study about "GraphicalView"
+	private XYSeries mCurrentSeries; // * series : set of numbers
 	private XYMultipleSeriesDataset mDataset = new XYMultipleSeriesDataset();
+	private SimpleSeriesRenderer mCurrentRenderer;
 	private XYMultipleSeriesRenderer mRenderer = new XYMultipleSeriesRenderer();
-	private XYSeries mCurrentSeries; // * series : 수열
-	private XYSeriesRenderer mCurrentRenderer;
 	
 	private void initChart(){
-		mCurrentSeries = new XYSeries("Power Rates");
+		mCurrentSeries = new XYSeries("kWh");
 		mDataset.addSeries(mCurrentSeries);
 		
 		mCurrentRenderer = new XYSeriesRenderer();
-		mRenderer.addSeriesRenderer(mCurrentRenderer);
+		mCurrentRenderer.setColor(Color.RED);
+		
+		
+		
+	    //mRenderer.setClickEnabled(false);
+
+		// double[] zoomlimits = new double[] {0,20,0,40}; // {zoomMinimumX, zoomMaximumX, zoomMinimumY, zoomMaximumY}
+		// mRenderer.setZoomLimits(zoomlimits);
+		mRenderer.setChartTitle("Graph of Power Rates");
+
+		int[] margins = new int[] {50,30,-100,0}; // {top, left, bottom, right}
+		mRenderer.setMargins(margins);
+		mRenderer.setChartTitleTextSize(40);
+		mRenderer.setAxisTitleTextSize(15);
+		//mRenderer.setBackgroundColor(Color.BLUE);
+		
+		mRenderer.setZoomEnabled(false, false); // * enable zoom
+		double[] range = new double[] {0,12,0,500};
+		mRenderer.setInitialRange(range);
+		mRenderer.setYLabelsAlign(Align.RIGHT);
+		mRenderer.setXTitle("Months");
+		mRenderer.setYTitle("kWh");
+		mRenderer.setShowGrid(true);
+		mRenderer.setGridColor(Color.GRAY);
+		mRenderer.setXLabels(0); // sets the number of integer labels to appear
+		mRenderer.addXTextLabel(0, "0");
+		mRenderer.addXTextLabel(1, "1");
+		mRenderer.addXTextLabel(2, "2");
+		mRenderer.addXTextLabel(3, "3");
+		mRenderer.addXTextLabel(4, "4");
+		mRenderer.addXTextLabel(5, "5");
+		mRenderer.addXTextLabel(6, "6");
+		mRenderer.addXTextLabel(7, "7");
+		mRenderer.addXTextLabel(8, "8");
+		mRenderer.addXTextLabel(9, "9");
+		mRenderer.addXTextLabel(10, "10");
+		mRenderer.addXTextLabel(11, "11");
+		mRenderer.addXTextLabel(12, "12");
+		mRenderer.setBarSpacing(0.8);
+		mRenderer.setXAxisMin(0.5);
+		mRenderer.setXAxisMax(12.5);
+		mRenderer.setYAxisMin(0);
+		mRenderer.setYAxisMax(500);
+	    mRenderer.addSeriesRenderer(mCurrentRenderer);
 	}
 	
 	private void addSampleData(){
@@ -66,6 +112,9 @@ public class Chart1 extends Activity {
 			addSampleData();
 			// * Study about "ChartFactory"
 			mChart = ChartFactory.getBarChartView(this, mDataset, mRenderer, Type.DEFAULT);
+			layout.addView(mChart);
+		} else{
+			mChart.repaint();
 		}
 	}
 }
